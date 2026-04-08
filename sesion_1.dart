6a1b721d9b733894b09701d045ba9592256db872
Amaury <amaury84@gmail.com>
2026-04-07 21:19:28 -0500

    feat: mas ejercicios

--- sesion_1.dart
+++ sesion_1.dart
@@ -3,7 +3,26 @@ void main(){
   print("hola $nombre bienvenido");
   //mostrar el nombre del usuario
   List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
-  numeros.forEach(print);
+  //numeros.forEach(print);
+  double sumar(double a, double b) {
+  return a + b;
+  }
+
+  var resultado = sumar(4.6,5.0);
+  print(resultado);
   
-  
+  void crearProducto({
+    required String nombre, 
+    int precio=0}){
+    print("producto $nombre con valor de $precio dólares. Creado desde la funcion void");
+    //return "Producto Creado";
+  }
+
+  crearProducto(nombre:"Portátil");
+  crearProducto(nombre:"Mouse");
+  crearProducto(nombre:"Pantalla",precio:1110);
+  crearProducto(nombre:"Teclado");
+
+
+
 }
