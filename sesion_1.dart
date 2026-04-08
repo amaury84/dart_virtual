d75cb439ec0006f1ddc74303c17a6cb29bd4b690
Amaury <amaury84@gmail.com>
2026-04-07 20:36:57 -0500

    feat: agregados números

--- sesion_1.dart
+++ sesion_1.dart
@@ -1,4 +1,7 @@
 void main(){
   String? nombre = "Amaury";
   print("hola $nombre bienvenido");
+  //mostrar el nombre del usuario
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+  numeros.forEach(print);
 }
