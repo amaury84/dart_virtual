518303749861e32811a6d79cb1c34e59a6e3133e
Amaury <amaury84@gmail.com>
2026-04-07 20:49:54 -0500

    feat: datos eliminados

--- sesion_1.dart
+++ sesion_1.dart
@@ -4,9 +4,6 @@ void main(){
   //mostrar el nombre del usuario
   List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
   numeros.forEach(print);
-  Map<String,String> datosSesion={
-    "loginUser":"amaury84",
-    "loginPass":"1234",
-  };
-  print(datosSesion["loginUser"]);
+  
+  
 }
